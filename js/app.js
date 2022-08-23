9f5197ac63b92a7bd9f52a0022d25c4a3a892552
AsifKarim7 <asifkarim275@gmail.com>
2022-08-23 15:49:33 +0600

    adding budget calculator

--- js/app.js
+++ js/app.js
@@ -20,3 +20,27 @@ function selectButton(name, button) {
         alert("You can't add more than five players");
     }
 }
+
+document.getElementById('calculate').addEventListener('click', function () {
+
+    const perPlayer = getInputFieldValueById('perBudget');
+
+    const list = document.getElementsByTagName('li').length;
+
+    const totalPlayerExpense = perPlayer * list;
+
+    setTextElementValueById('expense', totalPlayerExpense);
+})
+
+document.getElementById('calculate-total')
+    .addEventListener('click', function () {
+
+        const manager = getInputFieldValueById('manager-field');
+
+        const coach = getInputFieldValueById('coach-field');
+
+        const playerTotalCost = document.getElementById('expense').innerText;
+
+        const total = parseFloat(playerTotalCost) + manager + coach;
+        setTextElementValueById('total-cost', total);
+    })
