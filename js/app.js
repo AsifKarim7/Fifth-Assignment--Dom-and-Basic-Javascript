158dfd7308ac4e4f74b35d90e5d333693ed4eef4
AsifKarim7 <asifkarim275@gmail.com>
2022-08-23 15:48:25 +0600

    adding utilities

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
 let list = [];
-function selectButton(name) {
+function selectButton(name, button) {
 
     const playerName = document.getElementById(name).innerText;
 
@@ -10,6 +10,11 @@ function selectButton(name) {
         const li = document.createElement('li');
         li.innerText = playerName;
         listContainer.appendChild(li);
+        li.style.padding = '20px 0px 0px 20px';
+
+        const playerButton = document.getElementById(button);
+        playerButton.disabled = true;
+
     }
     else {
         alert("You can't add more than five players");
